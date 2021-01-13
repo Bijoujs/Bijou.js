1bcf83800e54a7db7e01bc9131fe71b775665d62
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-13 21:43:59 +0000

    Auto-commit

--- git.bat
+++
@@ -1,9 +0,0 @@
-while true; do
-  cd /workspace/tiny.js
-  showdown makehtml -i README.md -o README.html
-  terser --compress --mangle -o tiny-min.js -- tiny.js
-  git stage .
-  git commit -m "Auto-commit"
-  git push
-  sleep 1
-  done
