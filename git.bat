d04d307e7d7a9c738a97359225edaeb2725acc03
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 15:31:02 +0000

    git.bat
    Files changed: git.bat, Date: 2021-01-14 15:31:02, User: Explosion-Scratch, Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -5,7 +5,7 @@ function commit {
   prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- tiny.js
   git stage .
   COMMIT=$(git diff --name-only --cached)
-  DATE=$(date +"%F %H:%M:%S")git rev-parse --show-toplevel
+  DATE=$(date +"%F %H:%M:%S")
   USER=$(git config user.name)
   EMAIL=$(git config user.email)
   COMMIT_MSG=$"Files changed: ${COMMIT}, Date: ${DATE}, User: ${USER}, Email: ${EMAIL}"
