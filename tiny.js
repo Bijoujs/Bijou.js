7a1f0a43673de0114edb791a8d1c0c4fd58dc175
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 13:50:47 +0000

    Some changes

--- tiny.js
+++ tiny.js
@@ -590,7 +590,7 @@ let _temp = {
   },
   parseHTML: (string) => {
     const domparser = new DOMParser()
-
+    return domparser.parseFromString(string, mimeType)
   }
 }
 _temp = _temp.sortObj(_temp)
