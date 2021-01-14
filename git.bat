5b2f1f8fa3f2953d079ae6753aae634d25057c48
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 20:14:34 +0000

    M git.bat
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:  M git.bat
    Date: 2021-01-14 20:14:34
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -5,7 +5,6 @@ function commit {
     cd /workspace/bijou.js
     showdown makehtml -i README.md -o README.html
     terser --compress --mangle -o bijou-min.js -- bijou.js
-    prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
     git stage .
     DATE=$(date +"%F %H:%M:%S")
     USER=$(git config user.name)
