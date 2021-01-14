63ec4d6c88acb4b827729e236cda6c250baffdbf
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 13:18:48 +0000

    Some changes

--- git.bat
+++ git.bat
@@ -1,15 +1,9 @@
 while true; do
   cd /workspace/tiny.js
-  printf "\033c"
   showdown makehtml -i README.md -o README.html
-  printf "\033c"
   terser --compress --mangle -o tiny-min.js -- tiny.js
-  printf "\033c"
   git stage .
-  printf "\033c"
   git commit -m "Some changes"
-  printf "\033c"
   git push
-  printf "\033c"
   sleep 1
   done
