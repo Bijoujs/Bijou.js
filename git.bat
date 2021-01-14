9faeb4d833812a53f9e14c58d4f898ebd87475f3
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 16:10:20 +0000

    bijou-min.js git.bat
      _   _                _
     | |_(_)_ __  _   _   (_)___
     | __| | '_ \| | | |  | / __|
     | |_| | | | | |_| |_ | \__ \
      \__|_|_| |_|\__, (_)/ |___/
                  |___/ |__/
    Files changed: bijou-min.js
    git.bat
    Date: 2021-01-14 16:10:20
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -1,7 +1,7 @@
 function commit {
   cd /workspace/tiny.js
   showdown makehtml -i README.md -o README.html
-  terser --compress --mangle -o tiny-min.js -- tiny.js
+  terser --compress --mangle -o bijou-min.js -- bijou.js
   prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- tiny.js
   git stage .
   COMMIT=$(git diff --name-only --cached)
