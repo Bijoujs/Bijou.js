801e07344ca0dc99bf960f9538eae8c45e8ad954
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 13:24:15 +0000

    Some changes

--- git.bat
+++ git.bat
@@ -2,6 +2,7 @@ while true; do
   cd /workspace/tiny.js
   showdown makehtml -i README.md -o README.html
   terser --compress --mangle -o tiny-min.js -- tiny.js
+  prettier --single-quote --trailing-comma all
   git stage .
   git commit -m "Some changes"
   git push
