f1eda6f2858ef303b867bc888b4bc2eaedeaf24b
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 17:04:33 +0000

    README to HTML

--- git.bat
+++ git.bat
@@ -1,5 +1,19 @@
 function commit {
   cd /workspace/bijou.js
+  git stage .
+  COMMIT=$(git diff --name-only --cached)
+  DATE=$(date +"%F %H:%M:%S")
+  USER=$(git config user.name)
+  EMAIL=$(git config user.email)
+  REPO="Bijou.js"
+  FIGLET_REPO=$(figlet $REPO)
+  BR=$'\n'
+  COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${COMMIT}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
+  git commit -m "${COMMIT}" -m "${COMMIT_MSG}"
+  git push
+}
+function updateFiles {
+  cd /workspace/bijou.js
   showdown makehtml -i README.md -o README.html
   git stage .
   git commit -m "README to HTML"
@@ -11,17 +25,6 @@ function commit {
   prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
   git stage .
   git commit -m "Beautified files"
-  git push
-  git stage .
-  COMMIT=$(git diff --name-only --cached)
-  DATE=$(date +"%F %H:%M:%S")
-  USER=$(git config user.name)
-  EMAIL=$(git config user.email)
-  REPO="Bijou.js"
-  FIGLET_REPO=$(figlet $REPO)
-  BR=$'\n'
-  COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${COMMIT}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
-  git commit -m "${COMMIT}" -m "${COMMIT_MSG}"
   git push
 }
 while true; do
