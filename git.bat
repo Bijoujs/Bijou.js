bbf54ece05712d2da1c3ba235b67f6eb94ad7c7c
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 19:45:46 +0000

    README.html bijou.css git.bat
    Files changed: README.html
    bijou.css
    git.bat
    Date: 2021-01-14 19:45:46
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -1,20 +1,20 @@
+npm i -g showdown terser prettier
 function commit {
-  cd /workspace/bijou.js
-  showdown makehtml -i README.md -o README.html
-  terser --compress --mangle -o bijou-min.js -- bijou.js
-  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
-  git stage .
   COMMIT=$(git diff --name-only --cached)
-  DATE=$(date +"%F %H:%M:%S")
-  USER=$(git config user.name)
-  EMAIL=$(git config user.email)
-  REPO="BIJOU.JS"
-  FIGLET_REPO=$(figlet $REPO)
-  BR=$'\n'
-  COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${COMMIT}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
-  git commit -m "${COMMIT}" -m "${COMMIT_MSG}"
-  if -n $COMMIT
-  then
+  if [[ ${#COMMIT} -ge 1 ]] ; then
+    cd /workspace/bijou.js
+    showdown makehtml -i README.md -o README.html
+    terser --compress --mangle -o bijou-min.js -- bijou.js
+    prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
+    git stage .
+    DATE=$(date +"%F %H:%M:%S")
+    USER=$(git config user.name)
+    EMAIL=$(git config user.email)
+    REPO="BIJOU.JS"
+    FIGLET_REPO=$(figlet $REPO)
+    BR=$'\n'
+    COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${COMMIT}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
+    git commit -m "${COMMIT}" -m "${COMMIT_MSG}"
     git push
   fi
 }
