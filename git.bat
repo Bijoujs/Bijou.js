e66b9da6dc3680824dfc5a125d89f1f3169eed51
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-14 19:21:36 +0000

    git.bat
    Files changed: git.bat
    Date: 2021-01-14 19:21:36
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -1,32 +1,21 @@
 function commit {
   cd /workspace/bijou.js
+  showdown makehtml -i README.md -o README.html
+  terser --compress --mangle -o bijou-min.js -- bijou.js
+  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
   git stage .
   COMMIT=$(git diff --name-only --cached)
   DATE=$(date +"%F %H:%M:%S")
   USER=$(git config user.name)
   EMAIL=$(git config user.email)
-  REPO="Bijou.js"
+  REPO="BIJOU.JS"
   FIGLET_REPO=$(figlet $REPO)
   BR=$'\n'
   COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${COMMIT}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
   git commit -m "${COMMIT}" -m "${COMMIT_MSG}"
   git push
 }
-function updateFiles {
-  cd /workspace/bijou.js
-  showdown makehtml -i README.md -o README.html
-  git stage .
-  git commit -m "README to HTML"
-  git push
-  terser --compress --mangle -o bijou-min.js -- bijou.js
-  git stage .
-  git commit -m "Minified source"
-  git push
-  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
-  git stage .
-  git commit -m "Beautified files"
-  git push
-}
+
 while true; do
   commit
   done
