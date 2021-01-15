9c55e329330fb7b11915f605f2066b0f9b770f03
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-15 13:13:24 -0500

    Update git.bat

--- git.bat
+++ git.bat
@@ -2,7 +2,6 @@ sudo npm i -g showdown terser prettier figlet-cli
 function commit {
   COMMIT=$(git status --porcelain)
   if [[ ${#COMMIT} -ge 1 ]] ; then
-    cd /workspace/bijou.js
     showdown makehtml -i README.md -o README.html
     terser --compress --mangle -o bijou-min.js -- bijou.js
     COMMIT_FILE=$(git diff --name-only)
