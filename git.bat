8274ef62cc297415cf6459dd5a415b29eced3ffc
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-15 13:14:38 -0500

    Update git.bat

--- git.bat
+++ git.bat
@@ -18,10 +18,9 @@ function commit {
   fi
 }
 function beautify {
-  cd /workspace/bijou.js
   showdown makehtml -i README.md -o README.html
   terser --compress --mangle -o bijou-min.js -- bijou.js
-  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
+  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- ./
   commit
 }
 beautify
