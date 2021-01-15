6a5ad3b4bb235d585c6bffb3a455a4d27f94a02f
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2021-01-15 13:33:59 -0500

    Update git.bat

--- git.bat
+++ git.bat
@@ -2,6 +2,7 @@ sudo npm i -g showdown terser prettier figlet-cli
 function commit {
   COMMIT=$(git status --porcelain)
   if [[ ${#COMMIT} -ge 1 ]] ; then
+    cd /workspace/bijou.js
     showdown makehtml -i README.md -o README.html
     terser --compress --mangle -o bijou-min.js -- bijou.js
     COMMIT_FILE=$(git diff --name-only)
@@ -12,12 +13,16 @@ function commit {
     REPO="BIJOU.JS"
     FIGLET_REPO=$(figlet -kf Big -- $REPO)
     BR=$'\n'
+    COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${BR}${BR}${COMMIT}${BR}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
+    git commit -m "${COMMIT_FILE}" -m "${COMMIT_MSG}"
+    git push
   fi
 }
 function beautify {
+  cd /workspace/bijou.js
   showdown makehtml -i README.md -o README.html
   terser --compress --mangle -o bijou-min.js -- bijou.js
-  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- ./
+  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
   commit
 }
 beautify
