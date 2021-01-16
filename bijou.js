e89aff545160b28a1f5955eb2bc6cd327b03d54a
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-16 16:01:31 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-16 16:01:31
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1361,7 +1361,7 @@ let desc = {
   removeTags: "Returns an html string stripped of tags.",
   desktopOrMobile:
     "Returns whether the user is using a desktop or mobile device. (Uses user-agent sniffing which can be spoofed)",
-  camelCase: "Takes a string as an input and returns the "
+  camelCase: "Takes a string as an input and returns the camelCased certi"
 };
 _temp.info = (prop) => {
   return desc[prop];
