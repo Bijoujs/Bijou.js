10cc1f879cf9bf907d2cc7f07187c1936d2232db
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-16 23:04:46 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-16 23:04:46
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -17,7 +17,7 @@ This software comes with ABSOLUTELY NO WARRANTY and with the best intentions of 
   \____\___/|_| \_| |_| |_| \_\___|____/ \___/  |_| \___/|_| \_\____/
 -------------------------------------------------
 Contributors to Bijou.js:
-
+  - GrahamSH-
 
 (c) 2021 Explosion-Scratch, all rights reserved.
 
