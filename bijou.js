6f1adc9e60eeedd85e5e89f5427bc348b8fe27a8
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-16 23:01:13 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-16 23:01:13
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -6,7 +6,7 @@
 //  |____/___\___/ \___/ \___(_)/ |___/
 //                            |__/     
 // --------------------------------------------------
-// Bijou.js is copyright ()
+// Bijou.js is copyright (c) 2020
 let isNode = false;
 if (typeof window === "undefined" || typeof document === "undefined") {
   isNode = true;
