dc93e82dd0266768f50df37a348a9bb62f5352fc
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-16 15:59:57 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-16 15:59:57
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1269,7 +1269,7 @@ let _temp = {
       ? "mobile"
       : "desktop",
   removeTags: (html) => html.replace(/<[^>]*>/g, ""),
-  camelize: (str) {
+  camelCase: (str)=> {
     return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     }).replace(/\s+/g, '');
