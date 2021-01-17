2a7b62f6bf8f85ffc9f18393c33b9af301ec74ee
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-17 22:10:38 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-17 22:10:38
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -378,7 +378,9 @@ let _temp = {
       {}
     ),
   uuid: (seed = Math.random()) => {
-    if (typeof seed === "string") {}
+    if (typeof seed === "string") {
+      
+    }
     function _p8(s) {
       var p = (seed.toString(16) + "000000000").substr(2, 8);
       return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
