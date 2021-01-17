4f27fc4aba5384e238e604f19f508103d143e9e7
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-17 23:52:01 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-17 23:52:01
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -370,12 +370,13 @@ let _temp = {
       field.map(encodeURIComponent).join("=")
     ).join("&"),
   formToObject: (form) =>
-    {return Array.from(new FormData(form)).reduce(
+    Array.from(new FormData(form)).reduce(
       (acc, [key, value]) => ({
         ...acc,
         [key]: value,
       }),
-      {}},
+      {}
+    ),
   uuid: (seed = Math.random()) => {
     if (typeof seed === "string") {
       // Convert string to a number between 0 and 1
