a7122b7225f515fee3fed4f31812834272bcb604
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:27:41 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:27:41
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -606,7 +606,7 @@ let _temp = {
 * @param
 * @example
 * //
-* @returns {Object} The object of form data (The keys are the "name" atttributes of the form inputs and the values are the )
+* @returns {Object} The object of form data (The keys are the "name" atttributes of the form inputs and the values are the value)
 */
   formToObject: (form) => {
     if (isNode) {
