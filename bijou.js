44119ae4cea258978245dd2a36bf50befa122a6b
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:08:18 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:08:18
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -409,7 +409,7 @@ let _temp = {
  * @param {String} [delimiter=","]
  * @example
  * //
- * @returns {String} The string of comma seperated values (CSV)
+ * @returns {String} The string of comma seperated values (CSV) 
  */
   jsonToCsv: (arr, columns, delimiter = ",") =>
     [
