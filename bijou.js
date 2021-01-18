6144a1f3c68874032400f10c00250a2dcffddad7
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:30:49 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:30:49
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -791,7 +791,7 @@ let _temp = {
 */
   randomColor: () => "#" + Math.floor(Math.random() * 16777215).toString(16),
 /**
-* Lighten a color by a certain am
+* Lighten a color by a certain amoutn
 * @function
 * @memberOf bijou.js
 * @param
