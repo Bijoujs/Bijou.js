bf99fdfcaa3b120218b41e8decae348b93a2f79d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:09:10 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:09:10
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -408,7 +408,7 @@ let _temp = {
  * @param {String} columns 
  * @param {String} [delimiter=","]
  * @example
- * JSONtoCSV(
+ * _$(
   [{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }],
   ['a', 'b']
 ); // 'a,b\n"1","2"\n"3","4"\n"6",""\n"","7"'
