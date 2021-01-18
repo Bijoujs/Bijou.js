a423b8a2a874aa75d98c890e3d680d2efabe23ba
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 13:42:21 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 13:42:21
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1512,6 +1512,44 @@ let _temp = {
       Math.imul(h1 ^ (h1 >>> 13), 3266489909);
     return 4294967296 * (2097151 & h2) + (h1 >>> 0);
   },
+  blendColors: (color1, color2, percent) => {
+
+    const generateHex = (r, g, b) => {
+      let R = r.toString(16);
+      let G = g.toString(16);
+      let B = b.toString(16);
+
+      while (R.length < 2) {
+        R = `0${R}`;
+      }
+      while (G.length < 2) {
+        G = `0${G}`;
+      }
+      while (B.length < 2) {
+        B = `0${B}`;
+      }
+
+      return `#${R}${G}${B}`;
+    };
+
+    const mix = (start, end, percent) =>
+      start + (percent / 100) * (end - start);
+
+
+    const red1 = parseInt(`${color1[1]}${color1[2]}`, 16);
+    const green1 = parseInt(`${color1[3]}${color1[4]}`, 16);
+    const blue1 = parseInt(`${color1[5]}${color1[6]}`, 16);
+
+    const red2 = parseInt(`${color2[1]}${color2[2]}`, 16);
+    const green2 = parseInt(`${color2[3]}${color2[4]}`, 16);
+    const blue2 = parseInt(`${color2[5]}${color2[6]}`, 16);
+
+    const red = Math.round(mix(red1, red2, percent));
+    const green = Math.round(mix(green1, green2, percent));
+    const blue = Math.round(mix(blue1, blue2, percent));
+
+    return generateHex(red, green, blue);
+  }
 };
 // Sort the object
 _temp = _temp.sortObj(_temp);
