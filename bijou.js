d9f5307e0e9995758eeb72847bbf5f6f93227a8e
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:27:36 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:27:36
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -746,7 +746,7 @@ let _temp = {
 * @memberOf bijou.js
 * @param {Object} obj The object to sort.
 * @example
-* let obj = _$.sortObj({testing: "A value", anotherThing: "Another value!"});
+* let object = _$.sortObj({testing: "A value", anotherThing: "Another value!"});
 * @returns
 */
   sortObj: (obj) => {
