b96ab0960d34db044d7da6d8ecda726de2fe01da
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:07:07 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:07:07
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -404,7 +404,7 @@ let _temp = {
  * Returns the name of the weekday from the Date object specified.
  * @function
  * @memberOf bijou
- * @param {Array} array
+ * @param {Array} arra
  * @example
  * //
  * @returns {String} The day name from the date.
