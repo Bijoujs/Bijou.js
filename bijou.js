bfedf659c677393971bb7164db0ccb50d1dd3a1b
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:18:34 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:18:34
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -494,7 +494,7 @@ let _temp = {
 * @param {Array} arr The array to convert.
 * @param {String} [delimeter=,] The seperator (By default this is a comma.)
 * @example
-* _$.arrayToCS
+* _$.arrayToCSV()
 * @returns {String} The day name from the date.
 */
   arrayToCSV: (arr, delimiter = ",") =>
