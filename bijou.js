c0b18ed3bcb40d1fce2befc3b90daf308b4d84cf
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 15:13:43 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 15:13:43
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -221,7 +221,7 @@ let _temp = {
       document.getSelection().removeAllRanges();
       document.getSelection().addRange(selected);
     }
-    return 
+    return str.
   },
   throttle: (fn, wait) => {
     let inThrottle, lastFn, lastTime;
