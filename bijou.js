5a4393372b2517254352ef58bed05c0081127950
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:17:50 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:17:50
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -491,7 +491,7 @@ let _temp = {
 * Converts an array to CSV (Comma seperated values) data.
 * @function
 * @memberOf bijou
-* @param {Array}
+* @param {Array} array The array to convert.
 * @example
 * //
 * @returns {String} The day name from the date.
