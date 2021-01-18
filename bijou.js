83b8c4bdeb35803c8cb9cc8f02f6fa6c4c33f5d8
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:34:48 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:34:48
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -834,7 +834,7 @@ let _temp = {
 * @param
 * @example
 *
-* @returns {Object} An object that represents if the color is light or dark and how much. The object key "hsp" represents a vall
+* @returns {Object} An object that represents if the color is light or dark and how much. The object key "hsp" represents a value a
 */
   lightOrDark: (color) => {
     var r, g, b, hsp;
