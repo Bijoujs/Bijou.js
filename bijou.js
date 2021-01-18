a2c16576b85b6d56fbc7adaf02c1a8d74b2b8a7f
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:26:52 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:26:51
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -589,7 +589,7 @@ let _temp = {
 * @function
 * @memberOf bijou
 * @param {Element} form The form element.
-* @returns {String} The string of url queries (Excluding the hsotname)
+* @returns {String} The string of url queries (Excluding the hostname and path) 
 */
   serializeForm: (form) => {
     if (isNode) {
