a2e3a7f599d42802a26e9ab2bd21005f32f2f9ce
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:23:53 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:23:53
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -702,7 +702,7 @@ let _temp = {
 * @example
 * _$.replaceText(document.querySelector("div"), (text) => text.toUpperCase());
 * //Converts the text of the first <div> element to upperCase.
-* @returns {String} The te
+* @returns {String} The text to 
 */
   replaceText: (el, callback) => {
     if (isNode) {
