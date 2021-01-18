9020a709bc71531309b0950f7f0a8bf5031014e3
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:12:26 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:12:26
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -452,7 +452,7 @@ let _temp = {
 * @function
 * @memberOf bijou
 * @param {Array} array The array of items to run the callback with.
-* @param {Function}
+* @param {Function} callback The callback function
 * @example
 * //
 * @returns {String} The day name from the date.
