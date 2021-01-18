9b5cc1ad561e20eb0452e9a4ae202e7568a13543
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 14:50:04 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 14:50:04
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -141,7 +141,7 @@ let _temp = {
 * @param {element} el The element to add the styles to.
 * @param {Object} styles An object that represents the styles to be added. (camelCased) 
 * @example
-* _$.addStyles(document.documentElement, {})
+* _$.addStyles(document.documentElement, {background: })
 * @returns {Array}
 */
   addStyles: (el, styles) => {
