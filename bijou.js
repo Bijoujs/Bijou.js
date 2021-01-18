4e87ff3fb26165d154e5a8053fb23f9693d694c1
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 23:33:56 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 23:33:56
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1958,7 +1958,7 @@ let _temp = {
 * Easing functions
 * @Object
 * @memberOf bijou
-* @name
+* @namespace
 * @example
 * _$.ease.easeInOutQuad(.3);//Returns the eased point of about 1/3 along the animation.
 * @returns {Function} The easing function.
