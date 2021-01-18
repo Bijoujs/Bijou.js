ecf1bb0fea084207c8a59ca72f1e5f6c7a964ff6
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 14:35:47 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 14:35:47
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -70,7 +70,7 @@ if (isNode) {
 
 let _temp = {
 /**
-* Generates prime numbers to the inputted num
+* Generates prime numbers to the inputted
 * @param {num} title - The title of the book.
 */
   primesTo: (num) => {
