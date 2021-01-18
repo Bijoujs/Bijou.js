b8a6cec7dab0a2bdca30dc47689840942363f72e
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:38:52 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:38:52
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -874,7 +874,7 @@ let _temp = {
 * @param {Element} el The element whose styles to get.
 * @param {String} prop The css-property value to get of the styles.
 * @example
-* console.log(_$.compStyle(document.documentElement, "ba"))
+* console.log(_$.compStyle(document.documentElement, "background"))
 * @returns
 */
   compStyle: (el, prop) => {
