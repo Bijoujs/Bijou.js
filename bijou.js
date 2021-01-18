87afb924c2c1580af2210348411a06884f52769d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 23:32:39 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 23:32:39
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1952,7 +1952,7 @@ let _temp = {
       this.style.left = initX + event.clientX - mousePressX + "px";
       this.style.top = initY + event.clientY - mousePressY + "px";
     }
-    retur
+    return el.
   },
   ease: {
     // no easing, no acceleration
