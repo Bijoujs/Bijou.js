2e60204ed4c0a176cec9aea6bd1d7f0685e782b3
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 23:34:19 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 23:34:19
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1998,7 +1998,7 @@ let _temp = {
 * Gets JSON from a URL and performs a callback with it.
 * @function
 * @memberOf bijou
-* @param {String}
+* @param {String} url
 * @example
 * //
 * @returns {String}
