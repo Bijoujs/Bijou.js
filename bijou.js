f333fd499e8a23c99fb82fa35c1d7ce276d0a502
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:37:36 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:37:36
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -833,7 +833,7 @@ let _temp = {
 * @memberOf bijou.js
 * @param
 * @example
-* if (_$.lightOrDark("#333"))
+* if (_$.lightOrDark("#333333"))
 * @returns {Object} An object that represents if the color is light or dark and how much. The object key "hsp" represents a value out of 255 of how light the color is and the object's key "lightOrDark" is a string (Either "light" or "dark") of whether the color is light or dark.
 */
   lightOrDark: (color) => {
