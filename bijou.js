596ab24cc73b80b0b51059dad4d4e5b033727c7f
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:21:00 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:20:59
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -514,7 +514,7 @@ let _temp = {
 * @example
 * //Averages the array 1,2,3,4 after squaring each number.
 * _$.averageBy([1,2,3,4], (v) => v ** 2);
-* @returns {Number} The day name from the date.
+* @returns {Number} s
 */
   averageBy: (arr, fn) =>
     arr
