e5c802c0547d7c421e37aa825faca7ea7c608a1d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:23:13 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:23:13
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -700,7 +700,7 @@ let _temp = {
 * @param {Element} el The element to replace the text of.
 * @param {Function} callback The callback to run (Gets passed the element's text).
 * @example
-* _$.replaceText(document)
+* _$.replaceText(document.queryS)
 * @returns
 */
   replaceText: (el, callback) => {
