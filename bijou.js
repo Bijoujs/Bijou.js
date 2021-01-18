896c2bb291e5febeb042be4c870b9933bb371383
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 15:06:29 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 15:06:29
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -154,7 +154,7 @@ let _temp = {
 * Returns the callback when a a click is registered outside the selected element
 * @function
 * @param {element} element The element to use as the outsideclick element.
-* @param {fn}
+* @param {function}
 * @example
 * Example
 * @returns {Array}
