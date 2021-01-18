89e5f91e4051d1f4506ad06f51b9b90b12bf4551
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 16:49:16 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 16:49:16
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -394,7 +394,7 @@ let _temp = {
  * @param {String} locale The locale to use.
  * @example
  * _$.primesTo(100);//Returns an array of prime numbers up to 100.
- * @returns 
+ * @returns {String}
  */
   dayName: (date = 'en-US', locale) =>
     date.toLocaleDateString(locale, {
