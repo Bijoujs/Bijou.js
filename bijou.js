9063586d6fe0b11fab3f529ed82e48ae9c56f73f
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:24:22 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:24:22
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -722,7 +722,7 @@ let _temp = {
       o++
     )
       (e = t[o].nodeValue), (t[o].nodeValue = callback(e));
-    
+    returnm
   },
   timeFunction: (fn, name = "_ function timer") => {
     console.time(name);
