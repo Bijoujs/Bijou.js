f20f42adb332f8489c935e3eaf9f6cb5b947ca53
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:29:39 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:29:39
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -765,7 +765,7 @@ let _temp = {
 * @param {String} text The string to replace
 * @example
 * document.querySelector("h1").innerHTML = _$.widows(document.querySelector("h1").innerHTML);
-* //Replaces the last space in the <h1>'s inner
+* //Replaces the last space in the <h1>'s innerTExt 
 * @returns
 */
   widows: (text) => {
