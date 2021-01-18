a300d1d5edd96d4dc50fdab568eb390399ac5d70
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 17:10:43 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 17:10:42
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -431,7 +431,7 @@ let _temp = {
  * Joins two arrays together and removes duplicates.
  * @function
  * @memberOf bijou
- * @param
+ * @param {a}
  * @example
  * //
  * @returns {String} The day name from the date.
