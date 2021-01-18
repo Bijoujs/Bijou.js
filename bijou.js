a39253933e6cfb92c52444c961c5d7a4607dc301
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 23:29:51 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 23:29:50
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1888,7 +1888,7 @@ let _temp = {
       .replace(/\s+/g, "");
   },
 /**
-* Scrambles the order of characters in a string. Thanks to @\Touchcreator for the suggestion for this.
+* Scrambles the order of characters in a string. Thanks to \@Touchcreator for the suggestion for this.
 * @function
 * @memberOf bijou
 * @param {String} str The string to be scrambled
