48a33c5273283d6621cf311364a50c9ffbdba468
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 16:43:35 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 16:43:35
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -349,7 +349,7 @@ let _temp = {
  * Displays a desktop notification with the specified text.
  * @function
  * @memberOf bijou
- * @param {String}
+ * @param {String} text
  * @example
  * _$.primesTo(100);//Returns an array of prime numbers up to 100.
  * @returns
