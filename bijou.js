1a4c06a70a7467f51f6d6be1b2d4ab6598985371
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:23:43 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:23:43
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -701,7 +701,7 @@ let _temp = {
 * @param {Function} callback The callback to run (Gets passed the element's text).
 * @example
 * _$.replaceText(document.querySelector("div"), (text) => text.toUpperCase());
-* //Converts the text of the first <div> element to 
+* //Converts the text of the first <div> element to upperCase
 * @returns
 */
   replaceText: (el, callback) => {
