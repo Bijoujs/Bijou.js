1461a214482bbf3a0e17e6f2642facc782156e9d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 15:14:00 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 15:14:00
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -224,7 +224,7 @@ let _temp = {
     return str;
   },
 /**
-* Only runs the input function at MA
+* Only runs the input function at MAX the amount of 
 * @function
 * @param {number} num - Params
 * @example
