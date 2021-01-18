81f970434e9e344772ec746cf90fff94c6ba30e7
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:28:53 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:28:53
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -762,7 +762,7 @@ let _temp = {
 * Returns the last space in the string given replaced with "&nbsp;"
 * @function
 * @memberOf bijou.js
-* @param {String} text The 
+* @param {String} text The string to replace
 * @example
 *
 * @returns
