4d7ddf2b6c6ac3817b2869719a2ca9023cde5bc2
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 23:33:32 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 23:33:32
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -1959,7 +1959,7 @@ let _temp = {
 * @Object
 * @memberOf bijou
 * @example
-* _$.ease.easeInOutQuad(.3);//Returns the eased point of about 1/3 alo
+* _$.ease.easeInOutQuad(.3);//Returns the eased point of about 1/3 along the pa
 * @returns {String}
 */
   ease: {
