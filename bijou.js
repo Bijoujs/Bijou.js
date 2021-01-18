f824ffc33b5737043fe7d4da85e695b90c4ba264
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:19:11 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:19:11
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -391,7 +391,7 @@ let _temp = {
    * @function
    * @memberOf bijou
    * @param {Date} date The date object to use.
-   * @param {String} locale The locale to use.
+   * @param {String} [locale=e] The locale to use.
    * @example
    * _$.primesTo(100);//Returns an array of prime numbers up to 100.
    * @returns {String} The day name from the date.
