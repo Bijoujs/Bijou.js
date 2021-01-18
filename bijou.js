bf4e420c1c6958fd136325f7e2c900c26673ea78
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 20:34:02 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 20:34:02
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -828,7 +828,7 @@ let _temp = {
     return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
   },
 /**
-* Tests if a color 
+* Tests if a color is 
 * @function
 * @memberOf bijou.js
 * @param
