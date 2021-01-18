5fe28b84a9383203255b0e2928e2bec99cc0a056
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-18 15:11:11 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-18 15:11:11
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -174,7 +174,7 @@ let _temp = {
 * @param {Function} callback The callback to call when the user stops scrolling.
 * @example
 * _$.onScrollStop(() => {alert("You stopped scrolling!")})
-* @returns {Array}
+* @returns {}
 */
   onScrollStop: (callback) => {
     let isScrolling;
