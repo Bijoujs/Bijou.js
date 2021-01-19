6f4ba8765b32f1da6127512962ca4d024de8ba7f
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 21:49:05 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 21:49:05
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2167,7 +2167,7 @@ let _temp = {
     return matrix[b.length][a.length];
   },
     /**
-   * T
+   * Returns the size of a string in bye
    * @function
    * @memberOf bijou
    * @param {String} str
