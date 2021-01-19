101cf769d5c4e97cda4b24327cfab79f094dd713
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 22:24:54 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 22:24:53
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2241,7 +2241,7 @@ let _temp = {
       }
       return null;
     },
-    /
+    /*
     removeItem: (name) => {
       document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     }
