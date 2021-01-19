9b6e580f8fffaa0316ac67f192c2e90cd1c3cd57
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 22:23:04 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 22:23:03
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2213,7 +2213,7 @@ let _temp = {
     /**
      * @function
      * @param {String} name The name of the cookie to set
-     * 
+     * @param #
      */
     setItem: (name, value, days) => {
       var expires = "";
