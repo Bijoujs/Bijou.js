95d78c7df47abe9ff35b9a12de11fd1350de59d8
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 22:11:08 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 22:11:08
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2188,7 +2188,7 @@ let _temp = {
   */
   elementSiblings: n => [...n.parentElement.children].filter(c => c != n),
 /**
-* Preloads all of the image urls 
+* Preloads all of the image urls given in the arguments
 * @function
 * @memberOf bijou
 * @param {String} str
