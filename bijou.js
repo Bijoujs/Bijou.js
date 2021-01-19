9ed6b41db6314965a6023dbdeff37b9c6ad124f4
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 21:51:08 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 21:51:08
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2173,7 +2173,7 @@ let _temp = {
    * @param {String} str
    * @example
    * console.log(_$.byteSize("Hello world"));//Logs
-   * @returns {Number} The byte size of the s
+   * @returns {Number} The byte size of the string.
    */
   byteSize: (str) => new Blob([str]).size,
 };
