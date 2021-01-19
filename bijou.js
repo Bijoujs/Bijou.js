611ab37d641c070234e0ea660d29fddbdf8b7144
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 22:24:46 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 22:24:46
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2229,7 +2229,7 @@ let _temp = {
     /**
      * Gets a cookie from its name.
      * @function
-     * @param {}
+     * @param {String}
     */
     getItem: (name) => {
       var nameEQ = name + "=";
