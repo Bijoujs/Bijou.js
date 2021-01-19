cf3ff142b55aab17a5acbe8e0a01ade788a74766
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 22:25:28 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 22:25:27
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2243,7 +2243,7 @@ let _temp = {
     },
     /**
      * Deletes a cookie
-     * @param {String} name The 
+     * @param {String} name The name 
      */
     removeItem: (name) => {
       document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
