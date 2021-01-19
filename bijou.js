4fa9cdf1e540fa8076b96c0084956443e7262722
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 22:22:15 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 22:22:15
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2206,7 +2206,7 @@ let _temp = {
 * A set of functions to set and modify cookies.
 * @memberOf bijou
 * @example
-* _$.cookies.setItem("a_cookie", "Hello world!", )
+* _$.cookies.setItem("a_cookie", "Hello world!", 1);
 * @returns {Function} The function that the user wanted
 */
   cookies: {
