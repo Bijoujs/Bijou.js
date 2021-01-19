60352a332557877205a35f88db8a4c8a90a8d87d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 21:50:16 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 21:50:16
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2175,7 +2175,7 @@ let _temp = {
    * console.log(_$.byteSize("Hello world"));//Logs
    * @returns {String}
    */
-  byteSize: (str) => {}
+  byteSize: (str) => 
 };
 // Sort the object
 _temp = _temp.sortObj(_temp);
