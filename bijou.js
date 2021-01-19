3d94c5280212e203860f39f596fc9f0b4e9d54ab
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 22:24:04 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-19 22:24:04
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2211,7 +2211,7 @@ let _temp = {
 */
   cookies: {
     /**
-     * S
+     * Sets a cook
      * @function
      * @param {String} name The name of the cookie to set
      * @param {String} value The value of the cookie
