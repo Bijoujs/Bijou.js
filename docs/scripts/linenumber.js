cec3517c15cf71f187cfdf43d6e0e7771e55f00e
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-19 00:05:50 +0000

    [No-Actions] docs/bijou.html docs/bijou.js.html docs/global.html docs/index.html docs/scripts/linenumber.js docs/styles/jsdoc-default.css docs/styles/prettify-tomorrow.css
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M docs/scripts/linenumber.js
     M docs/styles/jsdoc-default.css
     M docs/styles/prettify-tomorrow.css
    
    Date: 2021-01-19 00:05:50
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- docs/scripts/linenumber.js
+++ docs/scripts/linenumber.js
@@ -1,5 +1,4 @@
 /*global document */
-<<<<<<< HEAD
 (function() {
     var source = document.getElementsByClassName('prettyprint source linenums');
     var i = 0;
@@ -8,16 +7,6 @@
     var lines;
     var totalLines;
     var anchorHash;
-=======
-(() => {
-    const source = document.getElementsByClassName('prettyprint source linenums');
-    let i = 0;
-    let lineNumber = 0;
-    let lineId;
-    let lines;
-    let totalLines;
-    let anchorHash;
->>>>>>> 968284b781ff9faa6cf44b4bdd395f0abe09b88b
 
     if (source && source[0]) {
         anchorHash = document.location.hash.substring(1);
@@ -26,11 +15,7 @@
 
         for (; i < totalLines; i++) {
             lineNumber++;
-<<<<<<< HEAD
             lineId = 'line' + lineNumber;
-=======
-            lineId = `line${lineNumber}`;
->>>>>>> 968284b781ff9faa6cf44b4bdd395f0abe09b88b
             lines[i].id = lineId;
             if (lineId === anchorHash) {
                 lines[i].className += ' selected';
