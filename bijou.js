0b9a6fec92ffa26705feb0d6a3f1e5d466d9b82f
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-20 19:29:05 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-20 19:29:05
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2312,7 +2312,7 @@ let _temp = {
      *  1 lowercase letter
      *  1 uppercase letter
      *  1 number 
-     * t least 8 characters long */
+     * At least 8 characters long */
     moderatePassword: /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/,
   },
 };
