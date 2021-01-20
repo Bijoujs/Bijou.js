363c8878f23a0e328eea3e4811e2c394f950657a
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-20 19:42:02 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-20 19:42:02
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2335,7 +2335,7 @@ let _temp = {
     /**Phone */
     phone: /^\+?[\d\s]{3,}$/,
   /**Credit cards */
-    visaCredit: /^4[0–9]{12}(?:[0–9]{3})?$/
+    visaCredit: /^4[0–9]{12}(?:[0–9]{3})?$/,
     expressCredit: /^3[47][0–9]{13}$/
   },
 };
