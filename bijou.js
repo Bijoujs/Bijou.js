1cb36aa1127d707456c6cf235ddf02cce41e493e
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-22 18:04:41 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-22 18:04:40
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2381,7 +2381,7 @@ let _temp = {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   },
   disableRightClick: (el) => { el.oncontextmenu = false; },
-  addBookmark: () => {
+  addBookmark: (url) => {
     if (window.sidebar) { // Mozilla Firefox Bookmark
       window.sidebar.addPanel(location.href, document.title, "");
     } else if (window.external) { // IE Favorite
