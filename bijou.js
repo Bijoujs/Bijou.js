400d29844ed1ad05d768ffff29b627a733ee9ec1
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-22 17:52:32 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-22 17:52:32
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2369,7 +2369,7 @@ let _temp = {
 * //If the website adress of the current page was "https://example.com/?q=hello&hello=world"
 * console.log(_$.urlQuery("hello"));//Returns "world";
 * //Or on a custom url:
-
+* ?
 * @returns {String}
 */
   urlQuery: (query, url = window.location.href) => {
