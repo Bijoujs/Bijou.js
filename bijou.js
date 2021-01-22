3fe64c2b15e670f9c5def7cb69ee1566005b51f3
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-22 18:02:40 +0000

    [No-Actions] bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-22 18:02:40
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2380,7 +2380,7 @@ let _temp = {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   },
-  disableRightClick: (el) => {}
+  disableRightClick: (el) => {el.on}
 };
 // Sort the object
 _temp = _temp.sortObj(_temp);
