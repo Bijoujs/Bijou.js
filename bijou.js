414e497c73da9c20c09053928fb8598c6dd8117c
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-22 17:51:59 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-22 17:51:59
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2366,7 +2366,7 @@ let _temp = {
 * @param {String} query The url query to get.
 * @param {String} [url=window.location.href] The url to find the query in. (By default this is the current url)
 * @example
-* //If the website adress of the current page was "https://example.com/?q=hello&hello=wor"
+* //If the website adress of the current page was "https://example.com/?q=hello&hello=world"
 * @returns {String}
 */
   urlQuery: (query, url = window.location.href) => {
