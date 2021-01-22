433c47f37bf8ff360f04ca6df01585fffaffa79c
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-22 17:51:02 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-22 17:51:01
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2364,7 +2364,7 @@ let _temp = {
 * @function
 * @memberOf bijou
 * @param {String} query The url query to get.
-* @param {String} [url=window.location.href] The url to find the 
+* @param {String} [url=window.location.href] The url to find the queries
 * @example
 * //
 * @returns {String}
