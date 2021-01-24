b6dc3439e2be19bee35f2fad0b482fec4cc38fef
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-24 18:14:54 +0000

    [No-Actions] bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-24 18:14:54
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2254,61 +2254,61 @@ let _temp = {
   */
   disableRightClick: (el) => { return el.oncontextmenu = false; },
   sanitizeHTML: (input) => {
-      var tagWhitelist_ = {
-        'A': true,
-        'B': true,
-        'BODY': true,
-        'BR': true,
-        'DIV': true,
-        'EM': true,
-        'HR': true,
-        'I': true,
-        'IMG': true,
-        'P': true,
-        'SPAN': true,
-        'STRONG': true
-      };
+    var tagWhitelist_ = {
+      'A': true,
+      'B': true,
+      'BODY': true,
+      'BR': true,
+      'DIV': true,
+      'EM': true,
+      'HR': true,
+      'I': true,
+      'IMG': true,
+      'P': true,
+      'SPAN': true,
+      'STRONG': true
+    };
 
-      var attributeWhitelist_ = {
-        'href': true,
-        'src': true
-      };
+    var attributeWhitelist_ = {
+      'href': true,
+      'src': true
+    };
 
     var iframe = document.createElement('iframe');
-if (iframe['sandbox'] === undefined) {
-  alert('Your browser does not support sandboxed iframes. Please upgrade to a modern browser.');
-  return '';
-}
-iframe['sandbox'] = 'allow-same-origin';
-iframe.style.display = 'none';
-document.body.appendChild(iframe); // necessary so the iframe contains a document
-iframe.contentDocument.body.innerHTML = input;
+    if (iframe['sandbox'] === undefined) {
+      alert('Your browser does not support sandboxed iframes. Please upgrade to a modern browser.');
+      return '';
+    }
+    iframe['sandbox'] = 'allow-same-origin';
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe); // necessary so the iframe contains a document
+    iframe.contentDocument.body.innerHTML = input;
 
-function makeSanitizedCopy(node) {
-  if (node.nodeType == Node.TEXT_NODE) {
-    var newNode = node.cloneNode(true);
-  } else if (node.nodeType == Node.ELEMENT_NODE && tagWhitelist_[node.tagName]) {
-    newNode = iframe.contentDocument.createElement(node.tagName);
-    for (var i = 0; i < node.attributes.length; i++) {
-      var attr = node.attributes[i];
-      if (attributeWhitelist_[attr.name]) {
-        newNode.setAttribute(attr.name, attr.value);
+    function makeSanitizedCopy(node) {
+      if (node.nodeType == Node.TEXT_NODE) {
+        var newNode = node.cloneNode(true);
+      } else if (node.nodeType == Node.ELEMENT_NODE && tagWhitelist_[node.tagName]) {
+        newNode = iframe.contentDocument.createElement(node.tagName);
+        for (var i = 0; i < node.attributes.length; i++) {
+          var attr = node.attributes[i];
+          if (attributeWhitelist_[attr.name]) {
+            newNode.setAttribute(attr.name, attr.value);
+          }
+        }
+        for (i = 0; i < node.childNodes.length; i++) {
+          var subCopy = makeSanitizedCopy(node.childNodes[i]);
+          newNode.appendChild(subCopy, false);
+        }
+      } else {
+        newNode = document.createDocumentFragment();
       }
-    }
-    for (i = 0; i < node.childNodes.length; i++) {
-      var subCopy = makeSanitizedCopy(node.childNodes[i]);
-      newNode.appendChild(subCopy, false);
-    }
-  } else {
-    newNode = document.createDocumentFragment();
-  }
-  return newNode;
-};
+      return newNode;
+    };
 
-var resultElement = makeSanitizedCopy(iframe.contentDocument.body);
-document.body.removeChild(iframe);
-return resultElement.innerHTML;
-},
+    var resultElement = makeSanitizedCopy(iframe.contentDocument.body);
+    document.body.removeChild(iframe);
+    return resultElement.innerHTML;
+  },
   /**
    * A set of functions to set and modify cookies.
    * @memberOf bijou
