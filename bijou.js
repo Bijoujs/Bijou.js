8781a645bb0ace66c58e852b568e6b9ab961fbab
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-24 18:45:26 +0000

    bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-24 18:45:26
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2329,7 +2329,7 @@ let _temp = {
 * //
 * @returns {undefined}
 */
-  saveBlob: (blob, fileName = "") => {
+  saveBlob: (blob, fileName = "output/t") => {
     var a = document.createElement("a");
     document.body.appendChild(a);
     a.style = "display: none";
