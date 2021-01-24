4f741518392bd6ccf9919844ae3a04bb5bf5bd72
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-24 18:42:02 +0000

    git.bat
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M git.bat
    
    Date: 2021-01-24 18:42:02
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -3,7 +3,7 @@ npm i -g showdown terser prettier figlet-cli
 function commit {
   COMMIT=$(git status --porcelain)
   if [[ ${#COMMIT} -ge 1 ]] ; then
-    cd /workspace/bijou.js
+    cd /workspace/Bijou.js
     showdown makehtml -i README.md -o README.html
     terser --compress --mangle -o bijou-min.js -- bijou.js
     COMMIT_FILE=$(git diff --name-only)
@@ -15,7 +15,7 @@ function commit {
     FIGLET_REPO=$(figlet -kf Big -- $REPO)
     BR=$'\n'
     COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${BR}${BR}${COMMIT}${BR}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
-    git commit -m "[No-Actions] ${COMMIT_FILE}" -m "${COMMIT_MSG}"
+    git commit -m "${COMMIT_FILE}" -m "${COMMIT_MSG}"
   fi
 }
 function beautify {
