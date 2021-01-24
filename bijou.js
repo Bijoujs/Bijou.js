f14a4e109ef9e9277237046e621f3e7d2a4719b2
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-24 18:50:36 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-24 18:50:36
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2346,7 +2346,6 @@ let _temp = {
 * @function
 * @memberOf bijou
 * @param {Object} object The object to clone.
-* 
 * @returns {undefined}
 */
   clone: (object) => JSON.parse(JSON.stringify(object)),
