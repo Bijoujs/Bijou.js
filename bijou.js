ba547f93b0c47e866630269b6c4ab257c2891ae4
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-24 18:49:19 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-24 18:49:19
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2327,7 +2327,7 @@ let _temp = {
 * @param {Blob} blob The blob to save as a file.
 * @param {String} [fileName=output.txt] The name of the output file (Must include the extension.)
 * @example
-* _$.saveBlob(new)
+* _$.saveBlob(new )
 * @returns {undefined}
 */
   saveBlob: (blob, fileName = "output.txt") => {
