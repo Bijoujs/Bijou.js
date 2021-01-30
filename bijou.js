db6b47575835d9efed7680dd053f0a568cef7b37
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-30 01:31:43 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-30 01:31:43
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2573,7 +2573,26 @@ let _temp = {
       return;
     }, duration);
   },
-  
+  var requestInterval = function (fn, delay) {
+    var requestAnimFrame = (function () {
+      return window.requestAnimationFrame || function (callback, element) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+    })(),
+      start = new Date().getTime(),
+      handle = {};
+    function loop() {
+      handle.value = requestAnimFrame(loop);
+      var current = new Date().getTime(),
+        delta = current - start;
+      if (delta >= delay) {
+        fn.call();
+        start = new Date().getTime();
+      }
+    }
+    handle.value = requestAnimFrame(loop);
+    return handle;
+  };
   /**
    * A set of functions to set and modify cookies.
    * @memberOf bijou
