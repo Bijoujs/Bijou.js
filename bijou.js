7a3b6632726f69c5672b81e0b9eecdf8a9964453
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-30 01:40:33 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-30 01:40:33
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2602,7 +2602,7 @@ let _temp = {
     return handle;
   },
 /**
- * Returns an array of objects representing the attributes of a passed elel
+ * Returns an array of objects representing the attributes of a passed element. F
  * @param {Element} el The HMTL element to get attributes from
  */
   attributes(el) {
