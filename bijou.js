b13ebf73f3075dcd488d3c8f7d1f9dc96cfac30d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-30 01:28:57 +0000

    bijou-min.js bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-30 01:28:57
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -66,6 +66,7 @@ Contributors to Bijou.js:
 /**
  * @description Tests if the user is using Node.js or not and throws an error in specific functions (that require the DOM) if they are.
  */
+le
 let isNode = false;
 if (typeof window === "undefined" || typeof document === "undefined") {
   isNode = true;
