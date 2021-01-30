c86078ee45f713950421b388d723e299f9886712
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-30 01:41:50 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-30 01:41:50
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2606,7 +2606,7 @@ let _temp = {
    * Returns an array of objects representing the attributes of a passed element. 
    * @param {Element} el The HMTL element to get attributes from.
    * @example
-   * console.log(Object.keys(_$.attributes(document.documentElement).join(""));
+   * console.log(Object.keys(_$.attributes(document.documentElement).join(", "));
    */
   attributes(el) {
     node();
