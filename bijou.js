27fdfb1a8532dd54c15441cb8136fe1faca08009
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-30 01:46:03 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-30 01:46:03
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2624,7 +2624,7 @@ let _temp = {
   },
   /**
    * Loads a script from a url (Can be to a local file or to a url) then funs a callback once it's loaded.
-   * @param {String} url 
+   * @param {String} url The url 
    */
   loadScript: (url, callback) => {
     node();
