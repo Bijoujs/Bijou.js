38031152bedb7e3b3b901d66d01c61ab75069b1b
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-30 01:44:48 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-30 01:44:48
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2622,7 +2622,25 @@ let _temp = {
     }
     return output;
   },
-  l
+  function loadScript(url, callback) {
+    var script = document.createElement("script")
+  script.type = "text/javascript";
+if (script.readyState) {  // only required for IE <9
+  script.onreadystatechange = function () {
+    if (script.readyState === "loaded" || script.readyState === "complete") {
+      script.onreadystatechange = null;
+      callback();
+    }
+  };
+} else {  //Others
+  script.onload = function () {
+    callback();
+  };
+}
+
+script.src = url;
+document.getElementsByTagName("head")[0].appendChild(script);
+}
   /**
    * A set of functions to set and modify cookies.
    * @memberOf bijou
