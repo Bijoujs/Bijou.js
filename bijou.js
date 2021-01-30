d500332bc2816f80b445a3d07ac70918b52c7526
Explosion-Scratch <explosion.scratch@gmail.com>
2021-01-30 01:39:29 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-01-30 01:39:29
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2600,6 +2600,7 @@ let _temp = {
     handle.value = requestAnimFrame(loop);
     return handle;
   },
+  /
   attributes(el) {
     var output = [];
     for (var att, i = 0, atts = el.attributes, n = atts.length; i < n; i++) {
