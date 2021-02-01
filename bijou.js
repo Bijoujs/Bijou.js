dfa02b68d29dcb2a954829a022316114373f2797
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:07:43 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:07:43
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2704,7 +2704,7 @@ let _temp = {
     return observer;
   },
   /**
-   * A lot like socket.io, this allows emit, on 
+   * A lot like socket.io, this allows emit, on and off 
    * @memberOf bijou
    * @function
    * @returns {Object} The object with the emit, on and off functions in it. 
