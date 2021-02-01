3eb3e5be2e4af7d42296130af4cd2b3eaf370b79
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:06:32 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:06:32
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2703,7 +2703,21 @@ let _temp = {
     );
     return observer;
   },
-  
+  let socket_clone = () => ({
+    hub: Object.create(null),
+    emit(event, data) {
+      (this.hub[event] || []).forEach(handler => handler(data));
+    },
+    on(event, handler) {
+      if (!this.hub[event]) this.hub[event] = [];
+      this.hub[event].push(handler);
+    },
+    off(event, handler) {
+      const i = (this.hub[event] || []).findIndex(h => h === handler);
+      if (i > -1) this.hub[event].splice(i, 1);
+      if (this.hub[event].length === 0) delete this.hub[event]
+    }
+  });
   /**
    * Fetches an image and runs the callback with the data url of the image.
    * @memberOf bijou
