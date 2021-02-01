6646d23bfa123cd5cfe2676b6f8cf6c702da872f
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:08:02 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:08:02
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2709,7 +2709,7 @@ let _temp = {
    * @function
    * @returns {Object} The object with the emit, on and off functions in it. 
    * @example
-   * let thing = _$.
+   * let thing = _$.hub
    */
   hub: () => ({
     hub: Object.create(null),
