1574bc88a8503462d08d28796a3e58e076c8b54d
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:23:20 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:23:20
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2736,7 +2736,7 @@ let _temp = {
   * @memberOf bijou
   * @function
   * @param {Function} val The function to test.
-* @returns {Boolean} True if the function is async and false if nto
+* @returns {Boolean} True if the function is async and false if not.
   */
   isAsync: val =>
     Object.prototype.toString.call(val) === '[object AsyncFunction]',
