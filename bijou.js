7fa9eddb6af9ab0a703525859cf271fed5a1b663
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:05:49 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:05:49
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2680,7 +2680,7 @@ let _temp = {
    * @param {Function} callback The callback function to run when a mutation happens.
    * @param {*} options The options to use. 
    * @example 
-   * _$.obs
+   * _$.observe
    * @returns {undefined}
    */
   observeMutations: (element, callback, options) => {
