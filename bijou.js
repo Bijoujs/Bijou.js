a379b932f6526eded737bf286874ab1c9a66ff3e
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:00:39 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:00:39
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2677,7 +2677,7 @@ let _temp = {
    * @memberOf bijou
    * @function
    * @param {Object} element The element to observe
-   * @param {Function} callback 
+   * @param {Function} callback The call
    */
   observeMutations: (element, callback, options) => {
     const observer = new MutationObserver(mutations =>
