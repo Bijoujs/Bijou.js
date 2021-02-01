f7fdbe4c3222a09bf05766040a183623f9434487
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 13:58:14 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 13:58:14
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2673,7 +2673,7 @@ let _temp = {
     return cached;
   },
   /**
-   * @memb
+   * @
    */
   observeMutations: (element, callback, options) => {
     const observer = new MutationObserver(mutations =>
