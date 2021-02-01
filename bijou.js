5b81dd841acd00b6adbff231cfcd72513dd52282
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:00:20 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:00:20
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2676,7 +2676,7 @@ let _temp = {
    * Observes the mutations of the object specified.
    * @memberOf bijou
    * @function
-   * @param {Element} element The element to ob
+   * @param {Element} element The element to observe
    */
   observeMutations: (element, callback, options) => {
     const observer = new MutationObserver(mutations =>
