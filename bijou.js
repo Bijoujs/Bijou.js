a3142d3036bf7b6c86142c83344e164d7f92c078
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-01 14:08:34 +0000

    bijou.js
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-01 14:08:34
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2712,7 +2712,7 @@ let _temp = {
    * let thing = _$.hub();
    * //Log any new data to the console
    * thing.on("data", (data) => console.log(data));
-   * setTimeout(, 2)
+   * setTimeout(, 2000)
    */
   hub: () => ({
     hub: Object.create(null),
