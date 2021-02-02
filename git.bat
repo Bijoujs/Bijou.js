4ab0628bad3e7a8ee2d6192b87a2b44dfe5e7f75
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-02 12:34:59 +0000

    bijou-min.js git.bat
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M git.bat
    
    Date: 2021-02-02 12:34:59
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -4,6 +4,8 @@ function commit {
   COMMIT=$(git status --porcelain)
   if [[ ${#COMMIT} -ge 1 ]] ; then
     cd /workspace/Bijou.js
+    eslint --fix ./
+    prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- ./
     showdown makehtml -i README.md -o README.html
     terser --compress --mangle -o bijou-min.js -- bijou.js
     COMMIT_FILE=$(git diff --name-only)
@@ -18,23 +20,7 @@ function commit {
     git commit -m "${COMMIT_FILE}" -m "${COMMIT_MSG}"
   fi
 }
-function beautify {
-  cd /workspace/bijou.js
-  showdown makehtml -i README.md -o README.html
-  terser --compress --mangle -o bijou-min.js -- bijou.js
-  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- ./
-  eslint --fix ./
-  COMMIT_FILE=$(git diff --name-only)
-  git stage .
-  DATE=$(date +"%F %H:%M:%S")
-  USER=$(git config user.name)
-  EMAIL=$(git config user.email)
-  REPO="BIJOU.JS"
-  FIGLET_REPO=$(figlet -kf Big -- $REPO)
-  BR=$'\n'
-  COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${BR}${BR}${COMMIT}${BR}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
-  git commit -m "${COMMIT_FILE}" -m "${COMMIT_MSG}"
-}
+
 while true; do 
   commit
   done
