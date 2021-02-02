288f6fa024138212417750d1ce81d30849a2c81e
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-02 12:45:07 +0000

    bijou.js docs/bijou.html docs/bijou.js.html docs/global.html docs/index.html
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-02 12:45:07
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -2852,7 +2852,7 @@ let _temp = {
    * ```
    * //JS
    * _$.context();
-   * //Now the user can corner click the items that have parents with a 
+   * //Now the user can corner click the items that have parents with a "contextmenu" attribute
    * @returns {undefined};
    */
 	context: () => {
