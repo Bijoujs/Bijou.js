1ad896212cc7abc581eb7c5b0d4ecd0c40d7eeb2
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-02 12:30:50 +0000

    git.bat
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
    Date: 2021-02-02 12:30:50
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -22,8 +22,18 @@ function beautify {
   cd /workspace/bijou.js
   showdown makehtml -i README.md -o README.html
   terser --compress --mangle -o bijou-min.js -- bijou.js
-  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- /workspace/
-  commit
+  prettier --quote-props=consistent --trailing-comma=all --no-semi --write -- ./
+  eslint --fix ./
+  COMMIT_FILE=$(git diff --name-only)
+  git stage .
+  DATE=$(date +"%F %H:%M:%S")
+  USER=$(git config user.name)
+  EMAIL=$(git config user.email)
+  REPO="BIJOU.JS"
+  FIGLET_REPO=$(figlet -kf Big -- $REPO)
+  BR=$'\n'
+  COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${BR}${BR}${COMMIT}${BR}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
+  git commit -m "${COMMIT_FILE}" -m "${COMMIT_MSG}"
 }
 while true; do 
   commit
