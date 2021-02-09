c6fd1df297f7945989f8bcde11209f28d9da47dc
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-09 13:57:52 +0000

    Fixes #54 bijou-min.js docs/bijou.html docs/bijou.js.html docs/global.html docs/index.html git.bat
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M git.bat
    
    Date: 2021-02-09 13:57:52
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -12,6 +12,8 @@ function commit {
     terser --compress --mangle -o bijou-min.js -- bijou.js
     eslint --fix ./
     prettier --write -- .
+    echo "Commit message?   "
+    read COMMIT_CUSTOM_MSG
     COMMIT_FILE=$(git diff --name-only)
     git stage .
     DATE=$(date +"%F %H:%M:%S")
@@ -21,7 +23,7 @@ function commit {
     FIGLET_REPO=$(figlet -kf Big -- $REPO)
     BR=$'\n'
     COMMIT_MSG=$"${FIGLET_REPO}${BR}Files changed: ${BR}${BR}${COMMIT}${BR}${BR}Date: ${DATE}${BR}User: ${USER}${BR}Email: ${EMAIL}"
-    git commit -m "${COMMIT_FILE}" -m "${COMMIT_MSG}"
+    git commit -m "${COMMIT_CUSTOM_MSG} ${COMMIT_FILE}" -m "${COMMIT_MSG}"
   fi
 }
 
