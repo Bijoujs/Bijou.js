a6ca5f72dd2e5543891a9183afdca99724543113
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-09 15:01:45 +0000

    Fixes #52 bijou.js docs/bijou.html docs/bijou.js.html docs/global.html docs/index.html
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou.js
    
    Date: 2021-02-09 15:01:45
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- bijou.js
+++ bijou.js
@@ -420,7 +420,13 @@ let _temp = {
    * _$.jsonToCsv(
     [{ a: 1, b: 2 }, { a: 3, b: 4, c: 5 }, { a: 6 }, { b: 7 }],
     ['a', 'b']
-  ); // 'a,b\n"1","2"\n"3","4"\n"6",""\n"","7"'
+  );
+  * //
+   a,b
+  "1","2"
+  "3","4"
+  "6",""
+  "","7"
    * @returns {String} The string of comma separated values (CSV) created from the JSON.
    */
   jsonToCsv: (arr, columns, delimiter = ',') =>
