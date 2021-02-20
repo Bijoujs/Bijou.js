89de5c7c35ad7e2c3fe55bc63487e106d5009851
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-20 16:32:06 +0000

    Babel and stuff bijou_node.js docs/docs/bijou.html docs/docs/bijou.js.html docs/docs/global.html docs/docs/index.html git.bat package-lock.json package.json
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     M bijou_node.js
     M docs/docs/bijou.html
     M docs/docs/bijou.js.html
     M docs/docs/global.html
     M docs/docs/index.html
     M git.bat
     M package-lock.json
     M package.json
    
    Date: 2021-02-20 16:32:06
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -1,6 +1,6 @@
 npm install;
-npm i -g eslint jsdoc prettier terser toast-jsdoc figlet-cli @babel/core @babel/cli @babel/preset-env @babel/plugin-transform-modules-commonjs
-npm install @babel/preset-env
+npm i -g eslint jsdoc prettier terser figlet-cli @babel/core @babel/cli @babel/preset-env @babel/plugin-transform-modules-commonjs
+npm install @babel/preset-env babel @babel/plugin-transform-arrow-functions @babel/plugin-transform-sticky-regex --no-save
 
 function remove {
   rm -f .git/index.lock
@@ -13,8 +13,8 @@ function commit {
   rm bijou-min.js
   eslint --fix ./
   prettier --write -- .
-  babel --plugins @babel/plugin-transform-modules-commonjs --presets=@babel/preset-env -o bijou_node.js -- bijou.js
-  terser --comments false --ecma 4 --ie8 --compress --drop-console --mangle -o bijou_node.js -- bijou_node.js
+  babel --plugins=@babel/plugin-transform-modules-commonjs,@babel/plugin-transform-arrow-functions,@babel/plugin-transform-sticky-regex -o bijou_node.js -- bijou.js
+  terser --mangle --compress --comments false -o bijou_node.js -- bijou_node.js
   jsdoc -c jsdoc.json
   babel --presets=@babel/preset-env -o bijou-min.js -- bijou.js
   terser --comments false --ecma 4 --ie8 --module --compress --drop-console --mangle -o bijou-min.js -- bijou-min.js
