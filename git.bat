a7d4e54691994c9e92ec45f464c56fd9628b1f9e
Explosion-Scratch <explosion.scratch@gmail.com>
2021-02-20 14:39:12 +0000

    Babel stuff and improvements to git.bat README.html bijou-min.js bijou_node.js docs/docs/bijou.html docs/docs/bijou.js.html docs/docs/global.html docs/docs/index.html git.bat package-lock.json package.json
      ____ _____     _  ____  _    _      _  _____
     |  _ \_   _|   | |/ __ \| |  | |    | |/ ____|
     | |_) || |     | | |  | | |  | |    | | (___
     |  _ < | | _   | | |  | | |  | |_   | |\___ \
     | |_) || || |__| | |__| | |__| | |__| |____) |
     |____/_____\____/ \____/ \____(_)____/|_____/
    
    Files changed:
    
     D README.html
     M bijou-min.js
     D bijou_node.js
     M docs/docs/bijou.html
     M docs/docs/bijou.js.html
     M docs/docs/global.html
     M docs/docs/index.html
     M git.bat
     M package-lock.json
     M package.json
    
    Date: 2021-02-20 14:39:12
    User: Explosion-Scratch
    Email: explosion.scratch@gmail.com

--- git.bat
+++ git.bat
@@ -1,20 +1,23 @@
 npm install;
-npm i -g eslint jsdoc prettier showdown terser toast-jsdoc figlet-cli @babel/core @babel/cli @babel/preset-env@ babel/plugin-transform-modules-commonjs
+npm i -g eslint jsdoc prettier showdown terser toast-jsdoc figlet-cli @babel/core @babel/cli @babel/preset-env @babel/plugin-transform-modules-commonjs
 function remove {
   rm -f .git/index.lock
 }
 function commit {
   COMMIT=$(git status --porcelain)
+  cd /workspace/Bijou.js
+  rm bijou_node.js
+  rm -r docs/docs
+  rm bijou-min.js
+  eslint --fix ./
+  prettier --write -- .
+  npm install @babel/preset-env
+  babel --plugins @babel/plugin-transform-modules-commonjs --presets=@babel/preset-env -o bijou_node.js -- bijou.js
+  terser --comments false --ecma 5 --ie8 --compress --drop-console --mangle -o bijou_node.js -- bijou_node.js
+  jsdoc -c jsdoc.json
+  babel --presets=@babel/preset-env -o bijou-min.js -- bijou.js
+  terser --comments false --ecma 5 --ie8 --module --compress --drop-console --mangle -o bijou-min.js -- bijou-min.js
   if [[ ${#COMMIT} -ge 1 ]] ; then
-    cd /workspace/Bijou.js
-    rm bijou_node.js
-    rm -r docs/docs
-    eslint --fix ./
-    prettier --write -- .
-    babel --plugins @babel/plugin-transform-modules-commonjs --presets=@babel/preset-env -o bijou_node.js -- bijou.js
-    terser --comments false --ecma 5 --ie8 --module --compress --mangle -o bijou_node.js -- bijou_node.js
-    jsdoc -c jsdoc.json
-    terser --comments false --ecma 5 --ie8 --module --compress --mangle -o bijou-min.js -- bijou.js
     echo "Commit message?   "
     read COMMIT_CUSTOM_MSG
     COMMIT_FILE=$(git diff --name-only)
